653623b8525cbb8a1ae66414b0b15fb3c602e92b
stragene <stragen2007@163.com>
2018-09-29 16:43:28 +0800

    rm ppp

--- USER/include/main.h
+++ USER/include/main.h
@@ -3,6 +3,17 @@
 
 #include "stm32f37x.h"
 
-
+/*
+产品ID：173278
+设备接入协议：MQTT
+APIKey：reC6LwIXkj6j9=AV5CCo8g6qijg=
+用户ID(user id):103931
+正式环境注册码:oLQKa1eMHAI1nvu3
+设备id：44325523
+鉴权信息：12345678
+设备API地址：http://api.heclouds.com/devices/44325523
+server Addr:183.230.40.39
+port:6002
+*/
 
 #endif
--- USER/source/main.c
+++ USER/source/main.c
@@ -7,9 +7,9 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
-#include "netif/ppp/pppos.h"
-#include "netif/ppp/pppapi.h"
-#include "lwip/tcpip.h"
+//#include "netif/ppp/pppos.h"
+//#include "netif/ppp/pppapi.h"
+//#include "lwip/tcpip.h"
 
 void vBoardInit(void);
 void vSim800_PPPInit(void);
@@ -18,10 +18,10 @@ void vTaskRunLed(void);
 void vTaskPPPRead(void);
 SemaphoreHandle_t xSemGprsRsvd;
 //QueueHandle_t handQueueU1Frame;
-static void status_cb(ppp_pcb *pcb, int err_code, void *ctx);
-static u32_t output_cb(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx);
-ppp_pcb *pppGprs;
-struct netif pppGprs_netif;
+//static void status_cb(ppp_pcb *pcb, int err_code, void *ctx);
+//static u32_t output_cb(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx);
+//ppp_pcb *pppGprs;
+//struct netif pppGprs_netif;
 
 void vSim800_PPPInit(void)
 {
@@ -38,10 +38,14 @@ void vSim800_PPPInit(void)
             pSim800GPRS->SendCmd("AT+CPIN?\r\n", "READY", 500, 3) &&
             pSim800GPRS->SendCmd("AT+CSQ\r\n", "", 500, 3) &&
             pSim800GPRS->SendCmd("AT+CGATT?\r\n", "", 500, 3) &&
-            /*pSim800GPRS->SendCmd("AT+CIPSHUT\r\n", "SHUT OK", 2000, 3) &&*/
-            pSim800GPRS->SendCmd("AT+CGDCONT=1,\"IP\",\"CMNET\"\r\n", "OK", 1000, 3)) // &&
+            pSim800GPRS->SendCmd("AT+CIPSHUT\r\n", "SHUT OK", 2000, 3) &&
+            pSim800GPRS->SendCmd("AT+CSTT=\"CMNET\"\r\n", "OK", 2000, 3) &&
+            pSim800GPRS->SendCmd("AT+CIICR\r\n", "OK", 90000, 3) &&
+            /*pSim800GPRS->SendCmd("AT+CGDCONT=1,\"IP\",\"CMNET\"\r\n", "OK", 1000, 3))&&*/
             /*pSim800GPRS->SendCmd("ATD*99#\r\n", "CONNECT", 2000, 3) &&*/
-            /*pSim800GPRS->SendCmd("AT+CIFSR\r\n", "", 2000, 3))*/
+            pSim800GPRS->SendCmd("AT+CIFSR\r\n", "", 2000, 3))
+            pSim800GPRS->SendCmd("AT+CIPSTART=\"TCP\",\"116.247.119.165\",9336\r\n", "CONNECT", 2000, 3))
+
             break;
         ;
     }
@@ -80,161 +84,4 @@ void vTaskRunLed(void)
         GPIO_ResetBits(GPIOA, GPIO_Pin_0);
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
-}
-
-void vTaskPPPRead(void)
-{
-    uint8_t databuf[BUF_SIZE];
-    uint8_t *pdatabuf = databuf;
-    uint32_t datalen;
-    for (;;)
-    {
-        if (pdTRUE == xSemaphoreTake(xSemGprsRsvd, portMAX_DELAY))
-        {
-            datalen = Uart_Read(pUartGPRS, pdatabuf, BUF_SIZE);
-            pppos_input_tcpip(pppGprs, pdatabuf, datalen);
-        }
-        vTaskDelay(500 / portTICK_RATE_MS);
-    }
-}
-/*************************************************
-*创建PPPGPRS接收任务,传递给内核:pppos_input_tcpip();
-*发送ATD*99
-*tcpip_init(NULL,NULL);
-*pppos_create()
-*ppp_connect()
-*ppp_set_default()
-*************************************************/
-void vSim800_PPP(void)
-{
-    uint8_t ctx = 0;
-    uint16_t holdoff;
-    pSim800GPRS->AutoReadEn();
-    xTaskCreate((void *)(vTaskPPPRead), "ppp_read", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    output_cb(pppGprs, (uint8_t *)"ATD*99#\r\n", 9, &ctx);
-    tcpip_init(NULL, NULL);
-    pppGprs = pppos_create(&pppGprs_netif, output_cb, status_cb, &ctx);
-    /*ppp_set_auth(pppGprs, PPPAUTHTYPE_ANY, "card", "card");*/
-    if (ERR_OK != ppp_connect(pppGprs, holdoff))
-    {
-        while (1)
-            ;
-    }
-    ppp_set_default(pppGprs);
-}
-static void status_cb(ppp_pcb *pcb, int err_code, void *ctx)
-{
-    struct netif *pppif = ppp_netif(pcb);
-    LWIP_UNUSED_ARG(ctx);
-    switch (err_code)
-    {
-    case PPPERR_NONE:
-    {
-        return;
-#if 0
-#if LWIP_DNS
-        const ip_addr_t *ns;
-#endif /* LWIP_DNS */
-        /*printf("status_cb: Connected\n");*/
-#if PPP_IPV4_SUPPORT
-/*
-*      printf("   our_ipaddr  = %s\n", ipaddr_ntoa(&pppif->ip_addr));
-*      printf("   his_ipaddr  = %s\n", ipaddr_ntoa(&pppif->gw));
-*      printf("   netmask     = %s\n", ipaddr_ntoa(&pppif->netmask));*/
-#if LWIP_DNS
-        ns = dns_getserver(0);
-        /*printf("   dns1        = %s\n", ipaddr_ntoa(ns));*/
-        ns = dns_getserver(1);
-        /*printf("   dns2        = %s\n", ipaddr_ntoa(ns));*/
-#endif /* LWIP_DNS */
-#endif /* PPP_IPV4_SUPPORT */
-#if PPP_IPV6_SUPPORT
-        /*printf("   our6_ipaddr = %s\n", ip6addr_ntoa(netif_ip6_addr(pppif, 0)));*/
-#endif /* PPP_IPV6_SUPPORT */
-#endif
-        break;
-    }
-    case PPPERR_PARAM:
-    {
-        //printf("status_cb: Invalid parameter\n");
-        break;
-    }
-    case PPPERR_OPEN:
-    {
-        //printf("status_cb: Unable to open PPP session\n");
-        break;
-    }
-    case PPPERR_DEVICE:
-    {
-        //printf("status_cb: Invalid I/O device for PPP\n");
-        break;
-    }
-    case PPPERR_ALLOC:
-    {
-        //printf("status_cb: Unable to allocate resources\n");
-        break;
-    }
-    case PPPERR_USER:
-    {
-        //printf("status_cb: User interrupt\n");
-        break;
-    }
-    case PPPERR_CONNECT:
-    {
-        //printf("status_cb: Connection lost\n");
-        break;
-    }
-    case PPPERR_AUTHFAIL:
-    {
-        //printf("status_cb: Failed authentication challenge\n");
-        break;
-    }
-    case PPPERR_PROTOCOL:
-    {
-        //printf("status_cb: Failed to meet protocol\n");
-        break;
-    }
-    case PPPERR_PEERDEAD:
-    {
-        //printf("status_cb: Connection timeout\n");
-        break;
-    }
-    case PPPERR_IDLETIMEOUT:
-    {
-        //printf("status_cb: Idle Timeout\n");
-        break;
-    }
-    case PPPERR_CONNECTTIME:
-    {
-        //printf("status_cb: Max connect time reached\n");
-        break;
-    }
-    case PPPERR_LOOPBACK:
-    {
-        //printf("status_cb: Loopback detected\n");
-        break;
-    }
-    default:
-    {
-        //printf("status_cb: Unknown error code %d\n", err_code);
-        break;
-    }
-    }
-    /* ppp_close() was previously called, don't reconnect */
-    if (err_code == PPPERR_USER)
-    {
-        /* ppp_free(); -- can be called here */
-        return;
-    }
-    /*
-   * Try to reconnect in 30 seconds, if you need a modem chatscript you have
-   * to do a much better signaling here ;-)
-   */
-    ppp_connect(pcb, 30);
-    /* OR ppp_listen(pcb); */
-}
-static u32_t output_cb(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx)
-{
-    //return Uart_OnceWrite(UART, data, len);
-    return pSim800GPRS->Send(data, len);
 }
